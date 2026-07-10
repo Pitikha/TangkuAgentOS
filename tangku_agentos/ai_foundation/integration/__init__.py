34bdfc2dcf460ab1df3f6c46dacaa8da29655351
@pitikha <133892745+Pitikha@users.noreply.github.com>
2026-07-10 14:24:56 +0530

    Create integration package

---
+++ tangku_agentos/ai_foundation/integration/__init__.py
@@ -0,0 +1,5 @@
+"""AI Foundation Framework - Integration"""
+from tangku_agentos.ai_foundation.integration.kernel import KernelIntegration
+from tangku_agentos.ai_foundation.integration.memory import MemoryIntegration
+from tangku_agentos.ai_foundation.integration.knowledge import KnowledgeIntegration
+__all__ = ["KernelIntegration", "MemoryIntegration", "KnowledgeIntegration"]
