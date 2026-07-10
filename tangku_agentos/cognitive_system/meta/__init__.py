4af6409d6ead29cae4c1bdec6c0c372ff68c4ff7
@pitikha <133892745+Pitikha@users.noreply.github.com>
2026-07-10 11:17:48 +0530

    Create meta package initialization

---
+++ tangku_agentos/cognitive_system/meta/__init__.py
@@ -0,0 +1,6 @@
+"""AI Cognitive System - Meta-Cognition Engines"""
+
+from tangku_agentos.cognitive_system.meta.self_monitoring import SelfMonitoringEngine
+from tangku_agentos.cognitive_system.meta.meta_cognition import MetaCognitionEngine
+
+__all__ = ["SelfMonitoringEngine", "MetaCognitionEngine"]
