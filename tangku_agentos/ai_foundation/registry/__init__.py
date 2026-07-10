a303205bd9e27cb11fd7f1abde3b45cd1cbb122c
@pitikha <133892745+Pitikha@users.noreply.github.com>
2026-07-10 12:15:39 +0530

    Create registry package

---
+++ tangku_agentos/ai_foundation/registry/__init__.py
@@ -0,0 +1,4 @@
+"""AI Foundation Framework - Registry"""
+from tangku_agentos.ai_foundation.registry.model_registry import ModelRegistry
+from tangku_agentos.ai_foundation.registry.capability_registry import CapabilityRegistry
+__all__ = ["ModelRegistry", "CapabilityRegistry"]
