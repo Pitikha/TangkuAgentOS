1a9f13d5e117ea2becf1997f200134279ee849e4
@pitikha <133892745+Pitikha@users.noreply.github.com>
2026-07-10 11:23:14 +0530

    Update memory package with all 5 memory engines

--- tangku_agentos/cognitive_system/memory/__init__.py
+++ tangku_agentos/cognitive_system/memory/__init__.py
@@ -2,5 +2,14 @@
 
 from tangku_agentos.cognitive_system.memory.working_memory import WorkingMemory
 from tangku_agentos.cognitive_system.memory.long_term_memory import LongTermMemoryInterface
+from tangku_agentos.cognitive_system.memory.episodic_memory import EpisodicMemoryInterface
+from tangku_agentos.cognitive_system.memory.semantic_memory import SemanticMemoryInterface
+from tangku_agentos.cognitive_system.memory.memory_consolidation import MemoryConsolidationEngine
 
-__all__ = ["WorkingMemory", "LongTermMemoryInterface"]
+__all__ = [
+    "WorkingMemory",
+    "LongTermMemoryInterface", 
+    "EpisodicMemoryInterface",
+    "SemanticMemoryInterface",
+    "MemoryConsolidationEngine"
+]
