6f59bdb38e00a0a812c09c6c5277eed9209557c5
@pitikha <133892745+Pitikha@users.noreply.github.com>
2026-07-10 11:28:43 +0530

    Create AI Foundation core package

---
+++ tangku_agentos/ai_foundation/core/__init__.py
@@ -0,0 +1,9 @@
+"""
+AI Foundation Framework - Core Components
+"""
+
+from tangku_agentos.ai_foundation.core.foundation import AIFoundation
+from tangku_agentos.ai_foundation.core.config import AIConfig
+from tangku_agentos.ai_foundation.core.exceptions import AIFoundationError
+
+__all__ = ["AIFoundation", "AIConfig", "AIFoundationError"]
