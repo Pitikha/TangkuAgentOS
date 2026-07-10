48a3339cb408a84d5b8cdfc830bbc976da7e62b7
@pitikha <133892745+Pitikha@users.noreply.github.com>
2026-07-10 11:45:45 +0530

    Create embeddings package

---
+++ tangku_agentos/ai_foundation/embeddings/__init__.py
@@ -0,0 +1,4 @@
+"""AI Foundation Framework - Embeddings"""
+from tangku_agentos.ai_foundation.embeddings.manager import EmbeddingManager
+from tangku_agentos.ai_foundation.embeddings.embedding import EmbeddingResult
+__all__ = ["EmbeddingManager", "EmbeddingResult"]
